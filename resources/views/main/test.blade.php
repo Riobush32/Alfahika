5558d9d9da9f90f83945297350ada803817d9c82
Riobush32 <riobush32@gmail,com>
2022-11-19 16:56:36 +0700

    first commit

---
+++ resources/views/main/test.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html data-theme="light">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    
+        @yield('content')
+    
+</body>
+
+</html>
